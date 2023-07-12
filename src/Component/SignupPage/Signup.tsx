b59ea3fbb44a4b20432c6d9d3f9f9ad15d46e0a5
hyun <pm6567@naver.com>
2023-07-13 00:48:02 +0900

    feat: Email required alert

--- src/Component/SignupPage/Signup.tsx
+++ src/Component/SignupPage/Signup.tsx
@@ -1,8 +1,38 @@
 import { Link } from "react-router-dom";
 import "./Signup.css";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+
+  const emailRegEx = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(ac\.kr)$/;
+  const emailRegEx2 = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(edu)$/;
+
+  const onEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailError(false);
+  };
+
+  const emailCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const emailValue = e.target.value;
+    const input = e.target as HTMLInputElement;
+
+    if (!emailValue) {
+      setEmailError(true);
+      input.setCustomValidity("이메일을 입력하여주세요");
+    } else if (!emailRegEx.test(emailValue) && !emailRegEx2.test(emailValue)) {
+      setEmailError(true);
+      input.setCustomValidity("유효한 이메일을 입력하여주세요");
+    } else {
+      setEmailError(false);
+      input.setCustomValidity("");
+      setEmail(emailValue);
+    }
+
+    input.reportValidity();
+  };
+
   return (
     <div className="SignupMain">
       <p className="SignupTitle">회원가입</p>
@@ -11,9 +41,14 @@ function Signup() {
         <div className="inputcontainer">
           <p className="label">이메일</p>
           <input
+            id="email"
             className="inputField"
             type="text"
-            placeholder="abc123@gwakkili.co.kr"
+            placeholder="abc123@gwakkili.ac.kr"
+            value={email}
+            onChange={onEmail}
+            onBlur={emailCheck}
+            required
           />
         </div>
       </div>
@@ -22,9 +57,10 @@ function Signup() {
         <div className="inputcontainer">
           <p className="label">비밀번호</p>
           <input
+            id="password"
             className="inputField"
             type="password"
-            placeholder="최소 6자 이상(알파벳,숫자 필수)"
+            placeholder="[영문 대, 소문자 1개 이상 + 숫자 1개 이상 + 특수문자 1개 이상]"
           />
         </div>
       </div>
@@ -32,9 +68,10 @@ function Signup() {
         <div className="inputcontainer">
           <p className="label">비밀번호 확인</p>
           <input
+            id="passwordChk"
             className="inputField"
             type="password"
-            placeholder="위의 비밀번호와 동일하게 작성하여주세요"
+            placeholder="위의 비밀번호와 동일하게 작성하세요"
           />
         </div>
       </div>
@@ -43,9 +80,10 @@ function Signup() {
         <div className="inputcontainer">
           <p className="label">닉네임</p>
           <input
+            id="nickname"
             className="inputField"
             type="text"
-            placeholder="4~15 이내로 입력해주세요"
+            placeholder="[3~20자의 영문(대,소문자), 한글, 숫자]"
           />
         </div>
       </div>
@@ -54,6 +92,7 @@ function Signup() {
         <div className="inputcontainer">
           <p className="label">학교</p>
           <input
+            id="universityName"
             className="inputField"
             type="text"
             placeholder="학교를 입력해주세요"
@@ -64,6 +103,7 @@ function Signup() {
         <div className="inputcontainer">
           <p className="label">학번</p>
           <input
+            id="sNo"
             className="inputField"
             type="text"
             placeholder="학번을 입력해주세요"
@@ -74,11 +114,9 @@ function Signup() {
       <div className="btndiv">
         <br />
 
-        <Link to="/SelectSch">
-          <button type="submit" className="Btn">
-            회원가입
-          </button>
-        </Link>
+        <button type="submit" className="Btn">
+          회원가입
+        </button>
       </div>
     </div>
   );
