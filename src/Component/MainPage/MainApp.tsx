85f9ee04653b6a8f0e0c54010ca30e40ce906125
yunjinsu <wlstn4940@gmail.com>
2023-07-12 23:58:23 +0900

    MainPage 게시판List 컴포넌트

--- src/Component/MainPage/MainApp.tsx
+++ src/Component/MainPage/MainApp.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Post from "./Post/Post";
+import PostList from "./MainPost/PostList";
 import MainHeaderSample from "./Header/MainHeader";
 
 import styles from "./MainApp.module.css";
@@ -8,10 +8,10 @@ function MainApp() {
   return (
     <div className={styles.layout}>
       {/* <MainHeader username="사용자" /> */}
-      <MainHeaderSample/>
+      <MainHeaderSample />
 
       <main className={styles.mainLayout}>
-        <div className={styles.emptybox}/>
+        <div className={styles.emptybox} />
         <div className={styles.flexbox1}>광고창</div>
         <div className={styles.flexbox2}>
           <div className={styles.hot}>
@@ -25,42 +25,23 @@ function MainApp() {
           </div>
 
           <div className={styles.box1}>
-            <div className={styles.post}>
-              <h3 className={styles.title}>자유게시판</h3>
-              <p className={styles.postContent}> 집가고싶다 </p>
-              <p> 로또 1등 당첨 </p>
-            </div>
+            <PostList />
 
-            <div className={styles.post}>
-              <h3 className={styles.title}>지식게시판</h3>
-              <p> flex 쓰는법 </p>
-            </div>
+            <PostList />
 
-            <div className={styles.post}>
-              <h3 className={styles.title}>QnA</h3>
-              <p> fetch로 어떻게 데이터 받아오나요?? </p>
-            </div>
+            <PostList />
           </div>
 
           <div className={styles.box2}>
-            <div className={styles.post}>
-              <h3 className={styles.title}>홍보게시판</h3>
-              <p> 프론트엔드좀 도와주실분 구합니다 </p>
-            </div>
+            <PostList />
 
-            <div className={styles.post}>
-              <h3 className={styles.title}>취업 | 진로</h3>
-              <p> 네카라쿠배 취업 </p>
-            </div>
+            <PostList />
 
-            <div className={styles.post}>
-              <h3 className={styles.title}>취미게시판</h3>
-              <p> 오타쿠 </p>
-            </div>
+            <PostList />
           </div>
         </div>
         <div className={styles.flexbox3}>광고창</div>
-        <div className={styles.emptybox}/>
+        <div className={styles.emptybox} />
       </main>
 
       <footer className={styles.footer}>Footer</footer>
