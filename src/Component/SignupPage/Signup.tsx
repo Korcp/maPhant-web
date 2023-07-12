fc0978877e02b6a4c75cfda3a704c4dd6ee20900
hyun <pm6567@naver.com>
2023-07-13 01:51:46 +0900

    feat: PassWord require alert

--- src/Component/SignupPage/Signup.tsx
+++ src/Component/SignupPage/Signup.tsx
@@ -4,20 +4,22 @@ import { useState } from "react";
 
 function Signup() {
   const [email, setEmail] = useState("");
-  const [emailError, setEmailError] = useState(false);
+  const [password, setPassword] = useState("");
 
+  //유효성검사
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  //이메일 유효성검사
   const emailRegEx = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(ac\.kr)$/;
   const emailRegEx2 = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(edu)$/;
-
   const onEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
     setEmailError(false);
   };
-
   const emailCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
     const emailValue = e.target.value;
     const input = e.target as HTMLInputElement;
-
     if (!emailValue) {
       setEmailError(true);
       input.setCustomValidity("이메일을 입력하여주세요");
@@ -29,7 +31,40 @@ function Signup() {
       input.setCustomValidity("");
       setEmail(emailValue);
     }
+    input.reportValidity();
+  };
 
+  //패스워드 유호성검사
+  const uppercaseRegex = /[A-Z]/;
+  const lowercaseRegex = /[a-z]/;
+  const numberRegex = /[0-9]/;
+  const specialCharRegex = /[!@#$%^&*()]/;
+  const onpassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    setPasswordError(false);
+  };
+
+  const passwordCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pwvalue = e.target.value;
+    const input = e.target as HTMLInputElement;
+
+    if (!pwvalue) {
+      setPasswordError(true);
+      input.setCustomValidity("비밀번호를 입력하여 주세요");
+    } else if (
+      !uppercaseRegex.test(pwvalue) ||
+      !lowercaseRegex.test(pwvalue) ||
+      !numberRegex.test(pwvalue) ||
+      !specialCharRegex.test(pwvalue)
+    ) {
+      input.setCustomValidity(
+        "비밀번호는 영문 대문자, 영문 소문자, 숫자, 특수문자를 모두 포함해야 합니다."
+      );
+    } else {
+      setPasswordError(false);
+      input.setCustomValidity("");
+      setPassword(pwvalue);
+    }
     input.reportValidity();
   };
 
@@ -61,6 +96,10 @@ function Signup() {
             className="inputField"
             type="password"
             placeholder="[영문 대, 소문자 1개 이상 + 숫자 1개 이상 + 특수문자 1개 이상]"
+            value={password}
+            onChange={onpassword}
+            onBlur={passwordCheck}
+            required
           />
         </div>
       </div>
