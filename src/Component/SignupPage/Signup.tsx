670afb7d6a1f9c821f01ab43106434565508d1bc
hyun <pm6567@naver.com>
2023-07-13 15:32:20 +0900

    feat: Signup Error Message all clear

--- src/Component/SignupPage/Signup.tsx
+++ src/Component/SignupPage/Signup.tsx
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Signup.css";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 
 function Signup() {
   //input에 입력되는 value값
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [Repassword, setRepassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const [school, setschool] = useState("");
+  const [sNo, setsNo] = useState("");
   //유효성검사
   const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
-
+  const [RepasswordError, setRepasswordError] = useState(false);
+  const [nicknameError, setNicknameError] = useState(false);
+  const [schoolError, setschoolError] = useState(false);
+  const [sNoError, setsNoError] = useState(false);
+  //주소 navigate
+  const navigate = useNavigate();
   //이메일 유효성검사
   const emailRegEx = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(ac\.kr)$/;
   const emailRegEx2 = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(edu)$/;
@@ -31,7 +39,7 @@ function Signup() {
     input.reportValidity();
   };
 
-  //패스워드 유호성검사
+  //비밀번호 유호성검사
   const uppercaseRegex = /[A-Z]/;
   const lowercaseRegex = /[a-z]/;
   const numberRegex = /[0-9]/;
@@ -49,17 +57,146 @@ function Signup() {
       !numberRegex.test(pwvalue) ||
       !specialCharRegex.test(pwvalue)
     ) {
+      setPasswordError(true);
       input.setCustomValidity(
         "비밀번호는 영문 대문자, 영문 소문자, 숫자, 특수문자를 모두 포함해야 합니다."
       );
     } else {
       setPasswordError(false);
       input.setCustomValidity("");
-      setPassword(pwvalue);
     }
     input.reportValidity();
   };
 
+  //비밀번호 확인 유호성검사
+  const onRepassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const repwvalue = e.target.value;
+    const input = e.target as HTMLInputElement;
+    setRepassword(repwvalue);
+    if (!repwvalue) {
+      setRepasswordError(true);
+      input.setCustomValidity("비밀번호 확인을 입력하여 주세요");
+    } else if (password != repwvalue) {
+      setRepasswordError(true);
+      input.setCustomValidity("위의 비밀번호와 동일하게 작성하여주세요");
+    } else {
+      setRepasswordError(false);
+      input.setCustomValidity("");
+    }
+    input.reportValidity();
+  };
+
+  //닉네임 확인 유효성 검사
+  const parttenRegex = /^[A-Za-z0-9가-힣]*$/;
+  const onNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nicknamevalue = e.target.value;
+    const input = e.target as HTMLInputElement;
+    setNickname(nicknamevalue);
+    if (!nicknamevalue) {
+      setNicknameError(true);
+      input.setCustomValidity("닉네임을 입력하여주세요");
+    } else if (
+      !parttenRegex.test(nicknamevalue) ||
+      nicknamevalue.length < 3 ||
+      nicknamevalue.length > 20
+    ) {
+      setNicknameError(true);
+      input.setCustomValidity(
+        "닉네임은 [3~20자의 영문(대,소문자), 한글, 숫자]만 입력가능합니다."
+      );
+    } else {
+      setNicknameError(false);
+      input.setCustomValidity("");
+    }
+    input.reportValidity();
+  };
+
+  //학교 유효성 검사
+  const onSchool = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const schoolvalue = e.target.value;
+    const input = e.target as HTMLInputElement;
+    setschool(schoolvalue);
+    if (!schoolvalue) {
+      setschoolError(true);
+      input.setCustomValidity("학교를 입력하여주세요");
+    } else {
+      setschoolError(false);
+      input.setCustomValidity("");
+    }
+    input.reportValidity();
+  };
+
+  //학번 유효성 검사
+
+  const onsNo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const sNovalue = e.target.value;
+    const input = e.target as HTMLInputElement;
+    setsNo(sNovalue);
+    if (!sNovalue) {
+      setsNoError(true);
+      input.setCustomValidity("학번을 입력하여 주세요");
+    } else if (!numberRegex.test(sNovalue)) {
+      setsNoError(true);
+      input.setCustomValidity("올바른 학번을 입력하여 주세요");
+    } else {
+      setsNoError(false);
+      input.setCustomValidity("");
+    }
+    input.reportValidity();
+  };
+
+  //회원가입 버튼 조건
+  const PassPage = () => {
+    if (!email) {
+      setEmailError(true);
+    }
+    if (!password) {
+      setPasswordError(true);
+    }
+
+    if (!Repassword) {
+      setRepasswordError(true);
+    }
+
+    if (!nickname) {
+      setNicknameError(true);
+    }
+
+    if (!school) {
+      setschoolError(true);
+    }
+
+    if (!sNo) {
+      setsNoError(true);
+    }
+    if (
+      emailError == true ||
+      passwordError == true ||
+      RepasswordError == true ||
+      nicknameError == true ||
+      schoolError == true ||
+      sNoError == true
+    ) {
+      alert("위의 빈칸을 입력해주시거나 조건에맞게 입력하여주세요");
+    }
+
+    if (
+      emailError === false &&
+      passwordError === false &&
+      RepasswordError === false &&
+      nicknameError === false &&
+      schoolError === false &&
+      sNoError === false
+    ) {
+      navigate("/SelectSch");
+    }
+    console.log(emailError);
+    console.log(passwordError);
+    console.log(RepasswordError);
+    console.log(nicknameError);
+    console.log(schoolError);
+    console.log(sNoError);
+  };
   return (
     <div className="SignupMain">
       <p className="SignupTitle">회원가입</p>
@@ -101,6 +238,9 @@ function Signup() {
             className="inputField"
             type="password"
             placeholder="위의 비밀번호와 동일하게 작성하세요"
+            value={Repassword}
+            onChange={onRepassword}
+            required
           />
         </div>
       </div>
@@ -113,6 +253,9 @@ function Signup() {
             className="inputField"
             type="text"
             placeholder="[3~20자의 영문(대,소문자), 한글, 숫자]"
+            value={nickname}
+            onChange={onNickname}
+            required
           />
         </div>
       </div>
@@ -125,6 +268,9 @@ function Signup() {
             className="inputField"
             type="text"
             placeholder="학교를 입력해주세요"
+            value={school}
+            onChange={onSchool}
+            required
           />
         </div>
       </div>
@@ -136,14 +282,16 @@ function Signup() {
             className="inputField"
             type="text"
             placeholder="학번을 입력해주세요"
+            value={sNo}
+            onChange={onsNo}
+            required
           />
         </div>
       </div>
 
       <div className="btndiv">
         <br />
-
-        <button type="submit" className="Btn">
+        <button type="submit" className="Btn" onClick={PassPage}>
           회원가입
         </button>
       </div>
