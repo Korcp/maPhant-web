e89ecf4b1893b248a0879cc8ade039e70bbeb674
hyun <pm6567@naver.com>
2023-07-17 17:56:44 +0900

    feat: Timer

--- src/Component/EmailCheck/EmailCheck.tsx
+++ src/Component/EmailCheck/EmailCheck.tsx
@@ -1,14 +1,20 @@
 import { Link } from "react-router-dom";
 import IconImg from "./img/Icon.png";
 import "./EmailCheck.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 function EmailCheck() {
   const [email, setEmail] = useState("");
+  const [time, setTime] = useState(180);
+  const [EmailCheck, setEmailCheck] = useState("");
+  const [showTimer, setShowTimer] = useState(false); // Added state variable
 
+  // 이메일 체크
   const [emailError, setEmailError] = useState(false);
   const [Click, setClick] = useState(false);
   const emailRegEx = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(ac\.kr)$/;
   const emailRegEx2 = /^[a-zA-Z0-9._%+-]+@([a-zA-Z0-9-]+\.)+(edu)$/;
+
   const onEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
     const emailValue = e.target.value;
     const input = e.target as HTMLInputElement;
@@ -29,9 +35,29 @@ function EmailCheck() {
   const ShowText = () => {
     if (!email || emailError) {
       setClick(false);
-    } else if (emailError == false) {
+    } else if (emailError === false) {
       setClick(true);
+      setShowTimer(true);
+      setTime(180);
     }
+  };
+
+  // 타이머
+  useEffect(() => {
+    if (time > 0) {
+      const timer = setTimeout(() => setTime(time - 1), 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [time]);
+
+  const formatTime = (seconds: number): string => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   return (
@@ -65,10 +91,15 @@ function EmailCheck() {
         <div className="email">
           <label className="EmailFont">인증번호</label>
           {Click && (
-            <label className="Check">
-              <br />
-              ＊이메일로 전송된 인증코드를 입력하여주세요.
-            </label>
+            <>
+              <label className="Check">
+                <br />
+                ＊이메일로 전송된 인증코드를 입력하여주세요.
+              </label>
+              {showTimer && (
+                <span className="Timer"> ※{formatTime(time)}</span>
+              )}{" "}
+            </>
           )}
           <br />
           <input
@@ -76,6 +107,8 @@ function EmailCheck() {
             type="email"
             id="emailId"
             placeholder="인증번호를 입력해주세요"
+            value={EmailCheck}
+            onChange={(e) => setEmailCheck(e.target.value)}
           />
           <br />
           <button className="submitbtn" type="submit">
@@ -85,7 +118,7 @@ function EmailCheck() {
 
         <div className="next">
           <Link to="/SelectSch">
-            <button type="submit" className="SelectBtn">
+            <button type="submit" className="EmailBtn">
               완료
             </button>
           </Link>
